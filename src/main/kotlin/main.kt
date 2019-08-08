21bc1f84a29c93fd8545f4483ce84b4486f92bcc
Shagen Ogandzhanian <shabunc@gmail.com>
2019-08-08 14:35:46 +0200

    an example borrowed from the original documentation for chalk

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,22 @@
 typealias TemplateStringsArray = Array<String>
 
 fun main() {
-    println(chalk.keyword("red")("KOTLIN"))
-    println(chalk.keyword("orange")("KOTLIN"))
-    println(chalk.keyword("yellow")("KOTLIN"))
-    println(chalk.keyword("green")("KOTLIN"))
-    println(chalk.keyword("blue")("KOTLIN"))
+
+    // Pass in multiple arguments
+    console.log(chalk.blue("Hello", "World!", "Foo", "bar", "biz", "baz"));
+
+    // Nest styles
+    console.log(chalk.red("Hello", chalk.underline.bgBlue("world") + "!"));
+
+    // Nest styles of the same type even (color, underline, background)
+    console.log(chalk.green(
+        "I am a green line " +
+                chalk.blue.underline.bold("with a blue substring") +
+                " that becomes green again!"
+    ));
+
+    // Use RGB colors in terminal emulators that support it.
+    console.log(chalk.keyword("orange")("Yay for orange colored text!"));
+    console.log(chalk.rgb(123, 45, 67).underline("Underlined reddish color"));
+    console.log(chalk.hex("#DEADED").bold("Bold gray!"));
 }
