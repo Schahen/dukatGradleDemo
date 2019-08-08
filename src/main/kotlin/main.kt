c35d55554f22b698542f64167ac2838e4ae81bde
Shagen Ogandzhanian <shabunc@gmail.com>
2019-08-08 13:53:32 +0200

    initial minimal setup

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+typealias TemplateStringsArray = Array<String>
+
+fun main() {
+    println(chalk.keyword("red")("KOTLIN"))
+    println(chalk.keyword("orange")("KOTLIN"))
+    println(chalk.keyword("yellow")("KOTLIN"))
+    println(chalk.keyword("green")("KOTLIN"))
+    println(chalk.keyword("blue")("KOTLIN"))
+}
