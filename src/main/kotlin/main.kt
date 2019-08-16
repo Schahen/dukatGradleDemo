8374e7ea46839a1e55a903f9345c8d08f83ad4c9
Shagen Ogandzhanian <shabunc@gmail.com>
2019-08-16 17:29:08 +0300

    Updated both gradle plugin and dukat versions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,24 @@
 typealias TemplateStringsArray = Array<String>
 
+
 fun main() {
 
+
     // Pass in multiple arguments
-    console.log(chalk.blue("Hello", "World!", "Foo", "bar", "biz", "baz"));
+    println(chalk.blue("Hello", "World!", "Foo", "bar", "biz", "baz"));
 
     // Nest styles
-    console.log(chalk.red("Hello", chalk.underline.bgBlue("world") + "!"));
+    println(chalk.red("Hello", chalk.underline.bgBlue("world") + "!"));
 
     // Nest styles of the same type even (color, underline, background)
-    console.log(chalk.green(
+    println(chalk.green(
         "I am a green line " +
                 chalk.blue.underline.bold("with a blue substring") +
                 " that becomes green again!"
     ));
 
     // Use RGB colors in terminal emulators that support it.
-    console.log(chalk.keyword("orange")("Yay for orange colored text!"));
-    console.log(chalk.rgb(123, 45, 67).underline("Underlined reddish color"));
-    console.log(chalk.hex("#DEADED").bold("Bold gray!"));
+    println(chalk.keyword("orange")("Yay for orange colored text!"));
+    println(chalk.rgb(123, 45, 67).underline("Underlined reddish color"));
+    println(chalk.hex("#DEADED").bold("Bold gray!"));
 }
