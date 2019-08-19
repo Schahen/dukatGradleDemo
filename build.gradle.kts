82704d606501be73ba4cc02a4dc2f1d19fadbd50
Shagen Ogandzhanian <shabunc@gmail.com>
2019-08-20 00:36:41 +0200

    Switch to kotlin gradle dsl

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.3.50-eap-86"
+}
+
+
+repositories {
+    mavenCentral()
+    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+}
+
+
+kotlin {
+    target {
+        useCommonJs()
+        nodejs()
+    }
+
+    sourceSets["main"].dependencies {
+        implementation(kotlin("stdlib-js"))
+        implementation(npm("chalk", "2.4.2"))
+    }
+}
+
+
+
+
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+
